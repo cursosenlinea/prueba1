a9df40c7f6e6ae8ad412bfa94df3f07749859f20
cursosenlinea <52253429+cursosenlinea@users.noreply.github.com>
2019-06-26 12:52:20 -0500

    Primera publicacion

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+
+<body>
+	<div id="contenedor">
+    	<header>
+			<h1>Diseño web-Responsive</h1>
+		</header>
+        
+        <nav>
+        	Menu
+        </nav>
+        
+        <section class="main">
+        	<img src="img/colibri.jpg">
+            <?php
+            	include("msj.txt");
+            ?>
+        </section>
+        
+        <aside>
+        	Información adicional
+
+            <?php
+            	include("msj.txt");
+            ?>
+            
+        </aside>
+        
+        <footer>
+        	Pie de pagina
+        </footer>
+	</div>
+</body>
+</html>
